b612d95acea7889518105055efa24a9c6f73edd9
Daniel Svitan <dankosaurus9@gmail.com>
2021-02-06 14:30:22 +0100

    making some changes

--- src/InputController.cpp
+++ src/InputController.cpp
@@ -1,4 +1,36 @@
-#pragma once
+//#pragma once
 #include <iostream>
+#include <conio.h>
 #include "InputController.h"
-#include <gainput/gainput.h>
+
+using namespace std;
+
+void Listener::listen(int (*callback)(char)) {
+    char key_press;
+
+    while (keep_listening) {
+        key_press = getch();
+        
+        callback(key_press);
+    }
+}
+
+void InputController::add_listener(int (*callback)(char)) {
+    if (listener != nullptr) {
+        cerr << "Error: Listener aleady exists" << endl;
+    }
+
+    Listener ls;
+    listener = &ls;
+    listener->keep_listening = true;
+    listener->listen(callback);
+}
+
+void InputController::remove_listener() {
+    if (listener == nullptr) {
+        cerr << "Error: Cant remove non existing listener" << endl;
+    }
+
+    listener->keep_listening = false;
+    listener = nullptr;
+}
--- src/InputController.h
+++ src/InputController.h
@@ -1,5 +1,17 @@
 #pragma once
 
-class InputController {
+class Listener {
+public:
+    bool keep_listening = false;
 
+    void listen(int (*callback)(char));
+};
+
+
+class InputController {
+private:
+    Listener* listener = nullptr;
+public:
+    void add_listener(int (*callback)(char));
+    void remove_listener();
 };
