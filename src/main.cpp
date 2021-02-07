c148b1f8869647fc9739437def95e00c998261eb
Daniel Svitan <dankosaurus9@gmail.com>
2021-02-07 10:39:38 +0100

    comment

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,11 @@ using namespace std;
 
 
 int main() {
-    //FieldBase field;
-    //field.field_init();
-    //field.output_field();
+    FieldBase field;
+    field.output_field();
 
 
-    cout << InputController::ask_input() << endl;
-
+    // cout << InputController::ask_input() << endl;
 
     return 0;
 }
